49463748808a8731f26cdf1880c43138666915c3
Diya Chandra <diyachandra807@gmail.com>
2025-10-30 08:42:10 +0000

    Day-1

---
+++ 590025852-Diya-001-Q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a;
+    int b;
+    printf("Please enter the numbers: ");
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    printf("The sum of the two numbers is %d\n",a+b);
+
+    return 0;
+}
